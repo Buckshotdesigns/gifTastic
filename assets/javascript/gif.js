8a89227aa3736e0f1e5539bb5238ccad37d4df5c
bryanbuckus <bryanbuckus@gmail.com>
2019-03-07 00:03:20 -0500

    styling and cleaning up javascript

--- assets/javascript/gif.js
+++ assets/javascript/gif.js
@@ -1,7 +1,7 @@
 
-
+// movie array global variable
 var movieArray = ["Pulp Fiction", "Die Hard", "The Fifth Element", "Joe Dirt", "The Running Man", "Commando", "Total Recall","The Expendables", "Snatch","Lock Stock and Two Smoking Barrels"];
-
+// function to create buttons from array
     function makeButtons() {
         $("#button-div").empty();
         for (var i = 0; i < movieArray.length; i++){
@@ -11,7 +11,6 @@ var movieArray = ["Pulp Fiction", "Die Hard", "The Fifth Element", "Joe Dirt", "
         gifButton.attr("data-movie", movieArray[i]);
         gifButton.text(movieArray[i]);
         $("#button-div").append(gifButton);
-
         };
     
         $(".movie-button").on("click", function() {
@@ -19,7 +18,7 @@ var movieArray = ["Pulp Fiction", "Die Hard", "The Fifth Element", "Joe Dirt", "
         $(".gif-images").remove();
         
         var movie = $(this).attr("data-movie");
-
+          // Start of api call to giphy
         var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + movie + "&api_key=OKuVGjgT0V7r20tpzmToIqOjeggIVUYM&limit=10";
         
         $.ajax({
@@ -47,14 +46,13 @@ var movieArray = ["Pulp Fiction", "Die Hard", "The Fifth Element", "Joe Dirt", "
                 // Creating an image tag
                 var movieImage = $("<img>");
 
-                // Giving the image tag an src attribute of a proprty pulled off the
-                // result item
+                // Giving the image tag an src attribute for still and animate
                 movieImage.attr("src", results[i].images.fixed_width_still.url);
                 movieImage.attr("data-still", results[i].images.fixed_width_still.url);
                 movieImage.attr("data-animate", results[i].images.fixed_width.url);
                 movieImage.attr("data-state", "still");
                 movieImage.addClass("gif");
-                // Appending the paragraph and personImage we created to the "gifDiv" div we created
+                // Appending the paragraph and movieImage we created to the "gifDiv" div we created
                 
                 gifDiv.append(movieImage);
                 gifDiv.append(p);
@@ -62,21 +60,13 @@ var movieArray = ["Pulp Fiction", "Die Hard", "The Fifth Element", "Joe Dirt", "
                 $("#gif-div").append(gifDiv);
 
                 }
-        
-
             }
         });
-
     });
-
 };
-
-   
-   
+    // adding the gifs when button is clicked
     $("#add-gif").on("click", function(event) {
         event.preventDefault();
-        console.log("ive been clicked");
-
         // This line will grab the text from the input box
         var movie = $("#movie-input").val().trim();
         if (movie === ""){
@@ -84,20 +74,13 @@ var movieArray = ["Pulp Fiction", "Die Hard", "The Fifth Element", "Joe Dirt", "
         }
         // The movie from the textbox is then added to our array
         movieArray.push(movie);
-        
         // calling makeButtons which handles the processing of our movie array
         makeButtons();
     });
-       
-    // makeButtons();
-
+      // making the gifs clickable to animate
     $(document).on("click", ".gif", function() {
-        console.log("ive been clicked");
-        // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
+        
         var state = $(this).attr("data-state");
-        // If the clicked image's state is still, update its src attribute to what its data-animate value is.
-        // Then, set the image's data-state to animate
-        // Else set src to the data-still value
         if (state === "still") {
           $(this).attr("src", $(this).attr("data-animate"));
           $(this).attr("data-state", "animate");
@@ -108,13 +91,3 @@ var movieArray = ["Pulp Fiction", "Die Hard", "The Fifth Element", "Joe Dirt", "
       });
 
       makeButtons();
-
-// start by creating an array of topics for the gif buttons
-// we then need to loop through that array and generate buttons
-// append the buttons to the html 
-// the api needs to be called to generate images 
-// images must be rated
-// these ratings also need to be displayed on the screen 
-// we need a search field to input user data with a button
-// once user has entered info it needs to generate a button by adding to original array
-// the loaded gifs need to be stills that when clicked animate 
